31ff01e894c7013f0b2fb5f718c9a6921248be53
Darpan Saini <darpansaini19978@gmail.com>
2026-01-22 12:21:34 +0530

    ADD: test_app.py file

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add
+
+def test_add_positive_numbers():
+    assert add(5, 3) == 8
+
+def test_add_zero():
+    assert add(0, 0) == 0
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
